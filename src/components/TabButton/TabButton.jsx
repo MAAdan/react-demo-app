3576182e7a3c8ded88bf48242fd1d07226e66a40
Miguel Adan <miguel.adan@kindredgroup.com>
2024-03-31 16:39:17 +0200

    Fix comment regarding forwarded props parameter

--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -1,6 +1,7 @@
 export default function TabButton({ children, onSelect, isSelected, ...props}) {
     return (
         <li>
+            {/* The onClick property that performs the action on this button is defined in Examples.jsx file and forwarded as part of the props parameter */}
             <button className={isSelected ? 'active' : undefined} {...props}>{children}</button>
         </li>
     );
